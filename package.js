8aaf53b4b638cb3d6808191f52458e043708468a
lojinmsleh111-stack <lojinmsleh111@gmail.com>
2026-08-10 14:56:51 +0400

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "alnatheem-roleplay-bot",
+  "version": "1.0.0",
+  "description": "Discord Bot for Roleplay Server",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.14.1",
+    "groq-sdk": "^0.3.0"
+  },
+  "author": "",
+  "license": "ISC"
+}
